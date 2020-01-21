3f218e8dfb4068a3833da63644e19b0812359cd8
Mrx-Anonym <60137544+Mrx-Anonym@users.noreply.github.com>
2020-01-21 15:18:40 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 clear
 echo "--------------------"
 echo "|    1. Termux     |"
-echo "|    2. Другое     |"
+echo "|    2. Other      |"
 echo "|                  |"
-echo "|     1 или 2:     |"
+echo "|      1 or 2:     |"
 echo "--------------------"
 read numb
 if [ $numb = "1" ]
@@ -14,25 +14,25 @@ then
 	pkg install python3-pip
 	pip3 install requests
 	pip3 install colorama
-	cp ~/alpha/spammer.py /data/data/com.termux/files/usr/bin/alpha
-	dos2unix /data/data/com.termux/files/usr/bin/alpha
-	chmod 777 /data/data/com.termux/files/usr/bin/alpha
-	alpha
+	cp ~/Mrx/spammer.py /data/data/com.termux/files/usr/bin/Mrx
+	dos2unix /data/data/com.termux/files/usr/bin/Mrx
+	chmod 777 /data/data/com.termux/files/usr/bin/Mrx
+	Mrx
 else
 	if [ $numb = "2" ]
 	then
 
 		if [ "$(whoami)" != 'root' ]; then
-			echo "You have no rights. Run install.sh with root (sudo sh ~/alpha/install.sh)"
+			echo "You have no rights. Run install.sh with root (sudo sh ~/Mrx/install.sh)"
 			exit
 		else
 			apt install python3 python3-pip
 			pip3 install requests
 			pip3 install colorama
-			cp ~/alpha/spammer.py /usr/bin/alpha
-			dos2unix /usr/bin/alpha
-			chmod 777 /usr/bin/alpha
-			alpha
+			cp ~/Mrx/spammer.py /usr/bin/Mrx
+			dos2unix /usr/bin/Mrx
+			chmod 777 /usr/bin/Mrx
+			Mrx
 		fi
 	else
 		echo "Invalid input"
