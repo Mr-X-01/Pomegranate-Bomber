05946ea9b51e0a36ab45af372e53d5ae6e7f1170
Mrx-Anonym <60137544+Mrx-Anonym@users.noreply.github.com>
2020-01-22 09:28:32 +0300

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -31,12 +31,12 @@ class Pomegranate:
 ###--####--#88888888888888888888888888#--####--###
 ##----------##########################+---------##
 ##################################################
-##################################################'''+Style.RESET_ALL+Fore.BLUE+'''
+##################################################'''+Style.RESET_ALL+Fore.RED+'''
 
 Before you start using this utility,
 we want to say - that you do everything at your own peril and risk!
 We are not responsible for what you do with this utility!
-'''+Style.RESET_ALL+Fore.RED+'''
+'''+Style.RESET_ALL+Fore.BLUE+'''
 [Write the phone number without "+"]
 ''')
 
