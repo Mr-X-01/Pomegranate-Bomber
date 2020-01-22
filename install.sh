62be60bec13d13cda7e7d053e10f3f7ec4decaff
Mrx-Anonym <60137544+Mrx-Anonym@users.noreply.github.com>
2020-01-22 18:53:44 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ then
 	pkg install python
 	pkg install python3
 	pkg install python3-pip
+	pkg install dos2unix
 	pip3 install requests
 	pip3 install colorama
 	cp ~/Pomegranate-Bomber/spammer.py /data/data/com.termux/files/usr/bin/Pomegranate-Bomber
